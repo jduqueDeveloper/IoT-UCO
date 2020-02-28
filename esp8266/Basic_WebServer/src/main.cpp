da3daae83a0dff5061754122396faa16dd48d71c
Emanuel <emanuel960411@gmail.com>
2020-02-27 21:06:38 -0500

    Se agregaron botones para encender el led interno del nodeMCU

--- esp8266/Basic_WebServer/src/main.cpp
+++ esp8266/Basic_WebServer/src/main.cpp
@@ -3,20 +3,61 @@
 #include <ESP8266WebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "xxxx";
-const char* password = "xxxxx";
+const char* ssid = "TP-LINK_3C02";
+const char* password = "camila1234";
 // WebServer Instantiation
 ESP8266WebServer server(80);
 
 //Web Page Variable
-String webPage = "";
+
+const char Server_Page[] PROGMEM = R"=====(
+<html>
+<body>
+<center>
+<h1></h1><br>
+<style>
+  .button{
+    color: white
+    padding: 20px 34px;
+    text-aling: center;
+    font-size: 20px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+
+  .slider {
+  -webkit-appearance: none;  /* Override default CSS styles */
+  appearance: none;
+  width: 100%; /* Full-width */
+  height: 25px; /* Specified height */
+  background: #d3d3d3; /* Grey background */
+  outline: none; /* Remove outline */
+  opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+  -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+  transition: opacity .2s;
+}
+
+</style>
+
+<form>
+  <button type = "submit" formaction = "/gpio1-on">Click me to do magic!</button>
+  <button type = "submit" formaction = "/gpio1-off">Click me to finish the magic!</button>
+</form>
+
+<div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+
+</center>
+</body>
+</html>
+
+)=====";
  
 void setup(void){
-  
-  //Build Basic Web Page using HTML
-  webPage += "<h1>AMAZING WEB SERVER</h1>";
-  webPage += "<p><marquee>######.....IoT Rulz!!!.....######</marquee></p>";
-  delay(1000);
+
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN, HIGH);
 
   //Serial Comunnication and Wifi Connection kick off
   Serial.begin(115200);
@@ -36,10 +77,16 @@ void setup(void){
   
   //Web Page handler on root "/" using .send() method from ESP8266WebServer class
   server.on("/", [](){
-    server.send(200, "text/html", webPage);
+    server.send(200, "text/html", Server_Page);
   });
-  server.on("/azul", [](){
-    server.send(200, "text/html", webPage += "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png'>");
+  server.on("/gpio1-on", [](){
+    digitalWrite(LED_BUILTIN, LOW);
+    server.send(200, "text/html", Server_Page);
+  });
+
+  server.on("/gpio1-off", [](){
+    digitalWrite(LED_BUILTIN, HIGH);
+    server.send(200, "text/html", Server_Page);
   });
 
   //Start Web Server and notify via serial comm.
