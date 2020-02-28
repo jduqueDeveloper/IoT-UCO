b08596964bcc15056dc49f23342a8b4dd98ed1cd
Juan Diego Cardona <jd99@outlook.com>
2020-02-27 21:23:26 -0500

    Update Esp8266-WebServer

---
+++ esp8266/Basic_WebServer/src/credentials.h
@@ -0,0 +1,14 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// ---------------- Wifi Credentials ------------------
+#define SSID "xxxxx"
+#define PASSWORD "xxxxx"
+//-----------------------------------------------------
+
+// ------------ Web page Credentials ------------------
+#define USER "xxxxx"
+#define USER_PASSWORD "xxxxx"
+//-----------------------------------------------------
+
+#endif
