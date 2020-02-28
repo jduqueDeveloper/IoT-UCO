d4d761154bc1e65af33ddfc221b25a4fe8ee5212
Luis Miguel <luismiguel010@gmail.com>
2020-02-27 23:35:53 -0500

    Login already

--- esp8266/Led_WebServer/src/main.cpp
+++ esp8266/Led_WebServer/src/main.cpp
@@ -3,11 +3,14 @@
 #include <ESP8266WebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "SEA UCO";
-const char* password = "SeaUco666";
+const char* ssid = "SATURNO";
+const char* password = "87654321";
 
 // WebServer Instantiation
 ESP8266WebServer server(80);
+
+const char* www_username = "admin";
+const char* www_password = "1234";
 
 #define LED D1
 
@@ -17,8 +20,7 @@ const char MAIN_page[] PROGMEM = R"=====(
 <center>
 <h1></h1><br>
 <a href="/ledMode"><img height="500" width="500" src="https://www.elektor.com/media/catalog/product/cache/2b4bee73c90e4689bbc4ca8391937af9/1/6/169034-91f-web.jpg" /></a>
-<a href="/ledProtoboard"><img height="540" width="380" src="https://pngimage.net/wp-content/uploads/2018/06/red-led-png-6.png" /></a>
-<hr>
+<a href="/ledProtoboard"><img height="540" width="480" src="https://pngimage.net/wp-content/uploads/2018/06/red-led-png-6.png" /></a>
 </center>
 </body>
 </html>
@@ -58,9 +60,15 @@ void setup(void){
   Serial.println(WiFi.localIP());
   
   //Web Page handler on root "/" using .send() method from ESP8266WebServer class
-  server.on("/", ledMode);
   server.on("/ledMode", ledMode);
   server.on("/ledProtoboard", ledProtoboard);
+
+  server.on("/", []() {
+    if (!server.authenticate(www_username, www_password)) {
+      return server.requestAuthentication();
+    }
+    server.send(200, "text/html", MAIN_page);
+  });
   
   //Start Web Server and notify via serial comm.
   server.begin();
